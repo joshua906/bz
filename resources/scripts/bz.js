c3af25ccf93895fba8c318c2673fac545153493f
joshua906 <joshuamatthews906@yahoo.com>
2016-10-22 21:50:33 -0500

    navigation changes

--- resources/scripts/bz.js
+++ resources/scripts/bz.js
@@ -1,3 +1,11 @@
 // JavaScript Document
 
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
