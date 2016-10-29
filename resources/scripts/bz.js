8e0f62c7aa443cac43f82ed062e18652677092c6
joshua906 <joshuamatthews906@yahoo.com>
2016-10-28 19:42:11 -0500

    animation loader and image changes

--- resources/scripts/bz.js
+++ resources/scripts/bz.js
@@ -18,6 +18,14 @@ function myFunction() {
 }
 
 
+$(document).ready(function() {
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		}, 500);
+});
+
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -33,14 +41,14 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
+  if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+      slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
